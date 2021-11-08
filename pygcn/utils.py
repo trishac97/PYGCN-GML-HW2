3be781781c10b7fa7edce9bf06652c2160d59c15
Trisha Chakraborty <68351305+trishac97@users.noreply.github.com>
2021-11-08 17:52:06 -0600

    Added symmetric normalization

--- pygcn/utils.py
+++ pygcn/utils.py
@@ -62,6 +62,14 @@ def normalize(mx):
     mx = r_mat_inv.dot(mx)
     return mx
 
+def normalize_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
 
 def accuracy(output, labels):
     preds = output.max(1)[1].type_as(labels)
